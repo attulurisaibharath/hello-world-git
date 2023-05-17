c673ea8d417bd734373ee18133630ad1f123b2b2
attulurisaibharath <130122865+attulurisaibharath@users.noreply.github.com>
2023-05-17 16:02:47 -0500

    Added extra info (#1)

--- src/main/java/com/career/helloworldgit/HelloWorldGitApplication.java
+++ src/main/java/com/career/helloworldgit/HelloWorldGitApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldGitApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloWorldGitApplication.class, args);
+		System.out.println("Hello Hey");
 	}
 
 }
